c106ca4225558f6eaf8be9a87b5b0e8ccc5f036c
Cengizhan Bozkurt <cenboz27@gmail.com>
2018-08-03 00:28:29 +0300

    Model Eklendi

--- IsBankasiSanalPos/Controllers/IsBankasiController.cs
+++ IsBankasiSanalPos/Controllers/IsBankasiController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IsBankasiSanalPos.Models.IsBankasi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,30 @@ namespace IsBankasiSanalPos.Controllers
         // GET: IsBankasi
         public ActionResult Index()
         {
-            return View();
+            IsBankasiVm model = new IsBankasiVm();
+
+            model.clientId = "700655000100";
+            model.amount = "9.95";
+            model.oid = "";
+            model.okUrl = "http://<SonucAdresi>";
+            model.failUrl = "http://<SonucAdresi>";
+            model.rnd = DateTime.Now.ToString();
+            model.storekey = "TRPS1234";
+            model.storetype = "3d";
+            String hashstr = model.clientId + model.oid + model.amount + model.okUrl + model.failUrl + model.rnd + model.storekey;
+            System.Security.Cryptography.SHA1 sha = new
+            System.Security.Cryptography.SHA1CryptoServiceProvider();
+            byte[] hashbytes = System.Text.Encoding.GetEncoding("ISO-8859-9").GetBytes(hashstr);
+            byte[] inputbytes = sha.ComputeHash(hashbytes);
+            model.hash = Convert.ToBase64String(inputbytes);
+            String description = "";
+            String xid = "";
+            String lang = "";
+            String email = "";
+            String userid = "";
+
+
+            return View(model);
         }
     }
 }
--- IsBankasiSanalPos/Models/IsBankasi/IsBankasiVm.cs
+++ IsBankasiSanalPos/Models/IsBankasi/IsBankasiVm.cs
@@ -7,5 +7,15 @@ namespace IsBankasiSanalPos.Models.IsBankasi
 {
     public class IsBankasiVm
     {
+        public string clientId { get; set; }
+        public string amount { get; set; }
+        public string oid { get; set; }
+        public string okUrl { get; set; }
+        public string failUrl { get; set; }
+        public string rnd { get; set; }
+        public string storekey { get; set; }
+        public string storetype { get; set; }
+
+        public string hash { get; set; }
     }
 }
