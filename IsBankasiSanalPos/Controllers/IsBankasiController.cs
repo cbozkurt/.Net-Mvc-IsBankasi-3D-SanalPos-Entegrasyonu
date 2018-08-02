faccf7c7942cea29fac9d404f9813be9c3c9c302
Cengizhan Bozkurt <cenboz27@gmail.com>
2018-08-03 00:18:24 +0300

    Model Eklendi

---
+++ IsBankasiSanalPos/Controllers/IsBankasiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IsBankasiSanalPos.Controllers
+{
+    public class IsBankasiController : Controller
+    {
+        // GET: IsBankasi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IsBankasiSanalPos/Models/IsBankasi/IsBankasiVm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IsBankasiSanalPos.Models.IsBankasi
+{
+    public class IsBankasiVm
+    {
+    }
+}
